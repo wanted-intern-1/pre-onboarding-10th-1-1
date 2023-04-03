81273c89479e956c0f9d5c8295a6d5eccfcfc818
Majesty-jun <hkwns01@gmail.com>
2023-04-03 14:23:10 +0900

    [feat] FormInput 컴포넌트 생성

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,64 @@
+import React, { useId } from 'react'
+import styled from 'styled-components';
+import { string } from 'prop-types';
+
+export function FormInput ({ testid, type, children, placeholder, name, ...restProps }) {
+  const id = useId();
+
+  return (
+    <>
+      <A11yHidden htmlFor={id}>{children}</A11yHidden>
+      <Input 
+        data-testid={testid}
+        type={type} 
+        id={id} 
+        placeholder={placeholder} 
+        name={name}
+        {...restProps}
+      />
+    </>
+  )
+}
+
+FormInput.propTypes = {
+  testid: string,
+  type: string,
+  children: string,
+  placeholder: string,
+  name: string
+}
+
+const Input = styled.input`
+  padding-left: 8px;
+  width: 240px;
+  height: 40px;
+  border: none;
+  border-bottom: 1px solid #000;
+  font-size: 16px;
+`;
+
+const A11yHidden = styled.label`
+  overflow: hidden;
+  position: absolute;
+  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: circle(0);
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+  white-space: nowrap;
+  
+  &:focus {
+    position: static;
+    clip: auto;
+    clip-path: unset;
+    width: initial;
+    height: initial;
+    margin: initial;
+    border: initial;
+    padding: initial;
+    white-space: initial;
+  }
+`
+
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export * from './PageTitle';
+export * from './FormInput';
+export * from './SubmitButton';
