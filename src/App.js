26eee02532e38bf882d1898a1192f5610cef7ee1
Majesty-jun <hkwns01@gmail.com>
2023-04-03 10:21:05 +0900

    [style] Reset style

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Todo from './pages/Todo';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
   return (
     <BrowserRouter>
+    <GlobalStyle />
       <Routes>
         <Route path='/' element={<></>}/>
         <Route path='/signin' element={<SignIn />}/>
