5dc5d5d8d5850e2ea3710d766547ca481e79f1fe
Majesty-jun <hkwns01@gmail.com>
2023-04-06 13:43:48 +0900

    [refactor] AuthForm 컴포넌트 리팩토링(useInput hook 제거)

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,63 +1,78 @@
-import React from 'react';
-import { useInput } from '@/hooks';
+import { bool, func } from 'prop-types';
 import styled from 'styled-components';
-import { useLocation } from 'react-router-dom';
 import { FormInput, SubmitButton } from '@/components';
+import React, { useEffect, useRef, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-export function AuthForm() {
+const initialFormState = {
+  email: '',
+  password: '',
+  passwordConfirm: ''
+};
+
+export function AuthForm({submitHandler, error}) {
   const location = useLocation();
-  const email = useInput();
-  const password = useInput();
+  const navigate = useNavigate();
+  const formRef = useRef(initialFormState);
+  const [hint, setHint] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  const currentPage = location.pathname === '/signup' ? 'SignUp' : 'SignIn';
 
-  const submitHandler = async (e) => {
-    console.log(e.target.email.value);
-    e.preventDefault();
-    const userInfo = {
-      email: e.target.email.value,
-      password: e.target.password.value
+  useEffect(() => {
+    if(localStorage.getItem('token')) navigate('/todo');
+    if(error) setHint('이메일 혹은 비밀번호를 확인해주세요.');
+  }, []);
+
+  const setValue = (name, value) => {
+    formRef.current[name] = value;
+    setHint('');
+  }
+
+  const resetValue = (name, hint) => {
+    formRef.current[name] = '';
+    setHint(hint);
+  }
+
+  const inputHandler = (e) => {
+    const {name, value} = e.target;
+    switch (name) {
+      case 'email':
+        if(value.includes('@')) setValue(name, value);
+        else resetValue(name, '이메일 형식에 맞게 입력해주세요.')
+        break;
+      case 'password':
+        if(value.length >= 8) setValue(name, value);
+        else resetValue(name, '8자 이상 입력해주세요.')
+        break;
+      case 'passwordConfirm':
+        if(Object.is(formRef.current.password, value)) setValue(name, value);
+        else resetValue(name, '비밀번호가 일치하지 않습니다.')
+        break;
     }
-    const res = await fetch('https://www.pre-onboarding-selection-task.shop/auth/signup', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(userInfo)
-    })
-    const data = await res;
-    console.log(data.status);
+    if(formRef.current.email && formRef.current.password) setDisabled(false);
+    else setDisabled(true);
   }
 
   return (
     <SignUpForm onSubmit={submitHandler}>
-      <FormInput testid="email-input" type="email" placeholder="Email" name="email" onChange={email.onInputChange}>
+      <FormInput testid="email-input" type="email" placeholder="Email" name="email" onInput={inputHandler}>
         이메일
       </FormInput>
-      {email.isValid ? null : <ValidationHint>{email.hint}</ValidationHint>}
-      <FormInput testid="password-input" type="password" placeholder="Password" name="password" onChange={password.onInputChange}>
+      <FormInput testid="password-input" type="password" placeholder="Password" name="password" onInput={inputHandler}>
         비밀번호
       </FormInput>
-      {password.isValid ? null : <ValidationHint>{password.hint}</ValidationHint>}
-      {
-        location.pathname === '/signup' ? 
-        <FormInput type="password" placeholder="PasswordConfirm" name="passwordConfirm">
-          비밀번호 확인
-        </FormInput> 
-        :
-        null
-      }
-      {location.pathname === '/signup' ?
-        <SubmitButton type="submit" disabled={!email.isValid || !password.isValid} testid="signup-button">
-          SignUp
-        </SubmitButton>
-        :
-        <SubmitButton type="submit" disabled={!email.isValid || !password.isValid} testid="signin-button">
-          SignIn
-        </SubmitButton>
-      }
-      
+      {hint ? <ValidationHint>{hint}</ValidationHint> : null}
+      <SubmitButton type="submit" disabled={disabled} testid={`${currentPage.toLowerCase()}-button`}>
+        {currentPage}
+      </SubmitButton>
     </SignUpForm>
   )
 }
+
+AuthForm.propTypes = {
+  submitHandler: func,
+  error: bool
+};
 
 const SignUpForm = styled.form`
   background: #fff;
@@ -71,6 +86,6 @@ const SignUpForm = styled.form`
 
 const ValidationHint = styled.span`
   position: absolute;
+  top: 60%;
   color: #F00;
-  top: 70%;
 `
