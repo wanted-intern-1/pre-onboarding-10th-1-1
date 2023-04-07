c524403f9f3dd92354e88042e05f9be92650e976
Majesty-jun <hkwns01@gmail.com>
2023-04-07 10:24:13 +0900

    [feat] 로그인 & 회원가입 용 Footer 생성

---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link, useLocation } from 'react-router-dom';
+
+export function Footer() {
+  const location = useLocation();
+  
+  return (
+    <>
+    {location.pathname === "/signup" ? 
+      <div>
+        <span>이미 회원이신가요?</span>
+        <Move to='/signin'>로그인</Move>
+      </div>
+        :
+      <div> 
+        <span>회원이 아니신가요?</span>
+        <Move to='/signup'>회원가입</Move>
+      </div>
+      }
+    </>
+  )
+}
+
+const Move = styled(Link)`
+  margin-left: 8px;
+  color: #fca311;
+  border-bottom: 2px solid #fca311;
+  font-weight: 800;
+`;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export * from './PageTitle';
 export * from './FormInput';
 export * from './SubmitButton';
+export * from './Footer';
+export * from './AuthForm';
