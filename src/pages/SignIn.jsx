9099df90380e73553775669956c6eb86ee76b804
Majesty-jun <hkwns01@gmail.com>
2023-04-10 11:13:05 +0900

    [feat] useFetch를 통한 로그인 기능 구현

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,58 @@
 import React from 'react'
+import { useFetch } from '@/hooks';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router';
+import { PageTitle, AuthForm, Footer } from '@/components';
+import { ReactComponent as Loading } from '@/assets/loading.svg';
 
 export default function SignIn() {
+  const navigate = useNavigate();
+  const {isLoading, error, data, status, fetchData} = useFetch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const userInfo = {
+      email: e.target.email.value,
+      password: e.target.password.value
+    }
+    fetchData('/auth/signin', {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(userInfo)
+    })
+  }
+
+  if(data && data.access_token) {
+    localStorage.setItem('token', data.access_token);
+    navigate('/todo');
+  }
+
+  if(isLoading) {
+    return (
+      <SignInSection>
+        <Loading alt="로딩중..."/>
+      </SignInSection>
+    )
+  }
+
   return (
-    <div>SignIn</div>
+    <SignInSection>
+      <PageTitle>SignIn</PageTitle>
+      <AuthForm submitHandler={submitHandler} error={error} status={status}/>
+      <Footer />
+    </SignInSection>
   )
 }
+
+const SignInSection = styled.section`
+  width: 360px;
+  height: 640px;
+  background-color: #fff;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  gap: 48px;
+  align-items: center;
+`
