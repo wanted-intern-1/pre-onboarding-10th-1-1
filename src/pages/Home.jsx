2752d1ce42f1b8f5055b62136a407adb49aa0514
Majesty-jun <hkwns01@gmail.com>
2023-04-10 11:57:02 +0900

    [fix] localStorage 키 값 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
 
 export default function Home() {
-  const token = localStorage.getItem('access_token');
+  const token = localStorage.getItem('token');
   const navigate = useNavigate();
 
   useEffect(() => {
