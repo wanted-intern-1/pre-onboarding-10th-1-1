419c1fe250ac617493de0165e5e06be4934264d5
Majesty-jun <hkwns01@gmail.com>
2023-04-13 15:05:49 +0900

    [feat] TodoList 컴포넌트 생성 및 변경사항 발생 시 비동기 통신 성공

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,47 @@
+import { useFetch } from '@/hooks';
+import { string } from 'prop-types';
+import styled from 'styled-components';
+import React, { useEffect, useState } from 'react';
+import { TodoItem, CreateTodo } from '@/components';
+
+export function TodoList({token}) {
+  const {data, fetchData} = useFetch(); 
+  const [reFetch, setReFetch] = useState(false);
+
+  useEffect(() => {
+    fetchData('/todos', {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    });
+  }, [reFetch]);
+
+  console.log(data);
+
+  return (
+    <>
+      <TodoListSection>
+      {
+        data ? 
+          <ul>
+            {data.map((todo) => {
+              return <TodoItem key={todo.id} data={todo} token={token} setReFetch={setReFetch}/>
+            })}
+          </ul>
+        : 
+        null
+      }
+      </TodoListSection>
+      <CreateTodo token={token} setReFetch={setReFetch}/>
+    </>
+  )
+}
+
+TodoList.propTypes = {
+  token: string,
+}
+
+const TodoListSection = styled.section`
+  margin-top: 120px;
+`
