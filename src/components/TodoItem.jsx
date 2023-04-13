b504b749d86d1a42ca1eecdee7c7a28ad716cafd
Majesty-jun <hkwns01@gmail.com>
2023-04-13 15:04:41 +0900

    [fix] checkbox checked 옵션 에러 수정

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+import React, { useId, useState } from 'react';
+import { func, object, string } from 'prop-types';
+import { UpdateTodo, DeleteTodo } from '@/components';
+
+export function TodoItem({data, token, setReFetch}) {
+  const id = useId();
+  const [isChecked, setIsChecked] = useState(data.isCompleted);
+
+  const completeHandler = () => {
+    setIsChecked((value) => !value);
+  }
+
+  return (
+    <Item>
+      <input type="checkbox" name="complete" id={id} checked={isChecked} onChange={completeHandler}/>
+      <Todo htmlFor={id} name="complete">{data.todo}</Todo>
+      <UpdateTodo data={data} isChecked={isChecked} token={token} setReFetch={setReFetch}/>
+      <DeleteTodo id={data.id} token={token} setReFetch={setReFetch} />
+    </Item>
+  )
+}
+
+TodoItem.propTypes = {
+  data: object,
+  token: string,
+  setReFetch: func
+}
+
+const Item = styled.li`
+  margin-top: 12px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: center;
+  width: 280px;
+  background-color: #fff;
+  font-size: 24px;
+`
+
+const Todo = styled.label`
+  flex-grow: 1;
+  max-width: 200px;
+  word-break: break-all;
+  padding-left: 8px;
+`
+
