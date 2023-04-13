af1a3fb87e8c9fffa43b4bc5ca40b91c1ec0d693
Majesty-jun <hkwns01@gmail.com>
2023-04-13 15:06:59 +0900

    [feat] Todo page 토큰 미확인 시 signin 페이지로 navigate

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { PageTitle, TodoList } from '@/components';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router';
 
 export default function Todo() {
+  const token = localStorage.getItem('token');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!token) navigate('/signin');
+  }, [])
+
   return (
-    <div>Todo</div>
+    <TodoSection>
+      <PageTitle>Todo List</PageTitle>
+      <TodoList token={token} />
+    </TodoSection>
   )
 }
+
+const TodoSection = styled.section`
+  width: 360px;
+  height: 640px;
+  background-color: #fff;
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 48px;
+  align-items: center;
+  position: relative;
+`
