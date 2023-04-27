6a3cfd042e0571ff5460e7aa1cf6ff52e5671fee
엄성현 <mingyov0807@gmail.com>
2023-04-27 23:13:14 +0900

    [refactor] : page component Lazy loading 최적화, React18v Suspense 컴포넌트 사용해서 loading 컴포넌트 전역처리

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { useState } from 'react';
-import Home from './pages/Home';
-import Todo from './pages/Todo';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import React, { useState, lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyle from './GlobalStyle';
 import { AccessTokenContext } from '@/context/TokenContext';
+
+const Home = lazy(() => import('./pages/Home'));
+const Todo = lazy(() => import('./pages/Todo'));
+const SignIn = lazy(() => import('./pages/SignIn'));
+const SignUp = lazy(() => import('./pages/SignUp'));
+const { Loading } = lazy(() => import('@/components'));
 
 function App() {
   const jwt = localStorage.getItem('token');
   const [token, setToken] = useState(jwt);
 
   return (
-    <AccessTokenContext.Provider value={{token, setToken}}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <GlobalStyle />
-        <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/signin' element={<SignIn />}/>
-          <Route path='/signup' element={<SignUp />}/>
-          <Route path='/todo' element={<Todo />}/>
-        </Routes>
-      </BrowserRouter>
-    </AccessTokenContext.Provider>
+    <Suspense fallback={Loading}>
+      <AccessTokenContext.Provider value={{ token, setToken }}>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <GlobalStyle />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/todo" element={<Todo />} />
+          </Routes>
+        </BrowserRouter>
+      </AccessTokenContext.Provider>
+    </Suspense>
   );
 }
 
