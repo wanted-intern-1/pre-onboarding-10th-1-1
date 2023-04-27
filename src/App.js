96387969e584bcdb28f73ac3f46ed7437d610299
정승덕 <akk9132@naver.com>
2023-04-27 15:59:57 +0900

    refactor: Add auth hoc for token verification

--- src/App.js
+++ src/App.js
@@ -6,20 +6,27 @@ import SignUp from './pages/SignUp';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyle from './GlobalStyle';
 import { AccessTokenContext } from '@/context/TokenContext';
+import withAuth from './hocs/withAuth';
 
 function App() {
   const jwt = localStorage.getItem('token');
   const [token, setToken] = useState(jwt);
 
+  // 각 페이지 컴포넌트에 withAuth HOC를 적용하여 인증 여부를 체크
+  const HomeWithAuth = withAuth(Home);
+  const SignInWithAuth = withAuth(SignIn);
+  const SignUpWithAuth = withAuth(SignUp);
+  const TodoWithAuth = withAuth(Todo);
+
   return (
-    <AccessTokenContext.Provider value={{token, setToken}}>
+    <AccessTokenContext.Provider value={{ token, setToken }}>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <GlobalStyle />
+        <GlobalStyle />
         <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/signin' element={<SignIn />}/>
-          <Route path='/signup' element={<SignUp />}/>
-          <Route path='/todo' element={<Todo />}/>
+          <Route path='/' element={<HomeWithAuth />} />
+          <Route path='/signin' element={<SignInWithAuth />} />
+          <Route path='/signup' element={<SignUpWithAuth />} />
+          <Route path='/todo' element={<TodoWithAuth />} />
         </Routes>
       </BrowserRouter>
     </AccessTokenContext.Provider>
