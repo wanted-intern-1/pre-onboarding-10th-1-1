03c67073d9262eb49d2e05bbc952d5b644bfe415
plu457 <ekgus5095@gmail.com>
2023-04-28 11:09:50 +0900

    refactor: 라우터, 리다이렉트 관련 코드 개선

--- src/App.js
+++ src/App.js
@@ -6,28 +6,18 @@ import SignUp from './pages/SignUp';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyle from './GlobalStyle';
 import { AccessTokenContext } from '@/context/TokenContext';
-import withAuth from './hocs/withAuth';
+
+import { ProtectedRoute } from './routes/ProtectedRoute';
+import Router from './routes/router';
 
 function App() {
   const jwt = localStorage.getItem('token');
   const [token, setToken] = useState(jwt);
-
-  // 각 페이지 컴포넌트에 withAuth HOC를 적용하여 인증 여부를 체크
-  const HomeWithAuth = withAuth(Home);
-  const SignInWithAuth = withAuth(SignIn);
-  const SignUpWithAuth = withAuth(SignUp);
-  const TodoWithAuth = withAuth(Todo);
-
   return (
     <AccessTokenContext.Provider value={{ token, setToken }}>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <GlobalStyle />
-        <Routes>
-          <Route path='/' element={<HomeWithAuth />} />
-          <Route path='/signin' element={<SignInWithAuth />} />
-          <Route path='/signup' element={<SignUpWithAuth />} />
-          <Route path='/todo' element={<TodoWithAuth />} />
-        </Routes>
+        <Router />
       </BrowserRouter>
     </AccessTokenContext.Provider>
   );
---
+++ src/routes/router.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from '@/pages/Home';
+import Todo from '@/pages/Todo';
+import SignIn from '@/pages/SignIn';
+import SignUp from '@/pages/SignUp';
+import { ProtectedRoute } from './ProtectedRoute';
+
+const Router = () => {
+  return (
+    <Routes element={<ProtectedRoute />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/todo" element={<Todo />} />
+    </Routes>
+  );
+};
+
+export default Router;
